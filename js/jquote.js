cee23d632a8304b5faf6cb8387a530b9336e4547
silencebow <29562312+silencebow@users.noreply.github.com>
2021-12-08 17:09:34 -0700

    Change script quotations to apostrophes

--- js/jquote.js
+++ js/jquote.js
@@ -3,4 +3,4 @@ var result = Math.floor(Math.random() * quotes.length);
 // Declare the quote as a variable
 var quote = quotes[result];
 // Insert the quote with quotation marks
-document.getElementById("quote").innerHTML = "\"" + quote + "\"";
+document.getElementById('quote').innerHTML = '"' + quote + '"';
