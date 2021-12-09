ddcf5e84ce0f0b8616934b95b6874d9be46f2e58
silencebow <29562312+silencebow@users.noreply.github.com>
2021-12-08 23:01:04 -0700

    Create tf2loadoutgenerator

--- js/tf2loadoutgenerator_array.js
+++ js/tf2loadoutgenerator_array.js
@@ -1,44 +1,53 @@
-var scout = {primary: ['Scattergun', 'Force-A-Nature', 'Shortstop', 'Soda Popper', 'Baby Face\'s Blaster', 'Back Scatter'],
+var scout = {name: 'Scout',
+             primary: ['Scattergun', 'Force-A-Nature', 'Shortstop', 'Soda Popper', 'Baby Face\'s Blaster', 'Back Scatter'],
              secondary: ['Pistol', 'Winger', 'Pretty Boy\'s Pocket Pistol', 'Flying Guillotine', 'Bonk! Atomic Punch', 'Crit-A-Cola', 'Mad Milk'],
              melee: ['Bat', 'Sandman', 'Candy Cane', 'Boston Basher', 'Sun-on-a-Stick', 'Fan O\'War', 'Atomizer', 'Wrap Assassin'],
              PDA: null
             }
-var soldier = {primary: ['Rocket Launcher', 'Direct Hit', 'Black Box', 'Rocket Jumper', 'Liberty Launcher', 'Cow Mangler 5000', 'Beggar\'s Bazooka', 'Air Strike'],
+var soldier = {name: 'Soldier', 
+               primary: ['Rocket Launcher', 'Direct Hit', 'Black Box', 'Rocket Jumper', 'Liberty Launcher', 'Cow Mangler 5000', 'Beggar\'s Bazooka', 'Air Strike'],
                secondary: ['Shotgun', 'Reserve Shooter', 'Buff Banner', 'Gunboats', 'Battalion\'s Backup', 'Concheror', 'Mantreads', 'Righteous Bison', 'B.A.S.E. Jumper', 'Panic Attack'],
-               melee: ['Shovel', 'Equalizer', 'Pain Train', 'Half-Zatoichi', 'Disciplinary Action', 'Market Gardner', 'Escape Plan'],
+               melee: ['Shovel', 'Equalizer', 'Pain Train', 'Half-Zatoichi', 'Disciplinary Action', 'Market Gardener', 'Escape Plan'],
                PDA: null
               }
-var pyro = {primary: ['Flame Thrower', 'Backburner', 'Degreaser', 'Phlogistinator', 'Dragon\'s Fury'],
+var pyro = {name: 'Pyro',
+            primary: ['Flame Thrower', 'Backburner', 'Degreaser', 'Phlogistinator', 'Dragon\'s Fury'],
             secondary: ['Shotgun', 'Reserve Shooter', 'Flare Gun', 'Detonator', 'Manmelter', 'Scorch Shot', 'Panic Attack', 'Thermal Thruster', 'Gas Passer'],
             melee: ['Fire Axe', 'Axtinguisher', 'Homewrecker', 'Powerjack', 'Back Scratcher', 'Sharpened Volcano Fragment', 'Third Degree', 'Neon Annihilator', 'Hot Hand'],
             PDA: null
           }
-var demoman = {primary: ['Grenade Launcher', 'Loch-n-Load', 'Ali Baba\'s Wee Booties', 'Loose Cannon', 'B.A.S.E. Jumper', 'Iron Bomber'],
+var demoman = {name: 'Demoman',
+               primary: ['Grenade Launcher', 'Loch-n-Load', 'Ali Baba\'s Wee Booties', 'Loose Cannon', 'B.A.S.E. Jumper', 'Iron Bomber'],
                secondary: ['Stickybomb Launcher', 'Scottish Resistance', 'Chargin\' Targe', 'Sticky Jumper', 'Splendid Screen', 'Tide Turner', 'Quickiebomb Launcher'],
-               melee: ['Bottle', 'Eyelander', 'Scotsman\'s Scullcutter', 'Pain Train', 'Ullapool Caber', 'Claidheamh Mòr', 'Half-Zatoichi', 'Persion Persuader'],
+               melee: ['Bottle', 'Eyelander', 'Scotsman\'s Skullcutter', 'Pain Train', 'Ullapool Caber', 'Claidheamh Mòr', 'Half-Zatoichi', 'Persian Persuader'],
                PDA: null
               }
-var heavy = {primary: ['Minigun', 'Natascha', 'Brass Beast', 'Tomislav', 'Huo-long Heater'],
+var heavy = {name: 'Heavy',
+             primary: ['Minigun', 'Natascha', 'Brass Beast', 'Tomislav', 'Huo-long Heater'],
              secondary: ['Shotgun', 'Family Business', 'Sandvich', 'Dalokohs Bar', 'Buffalo Steak Sandvich', 'Panic Attack', 'Second Banana'],
              melee: ['Fists', 'Killing Gloves of Boxing', 'Gloves of Running Urgently', 'Warrior\'s Spirit', 'Fists of Steel', 'Eviction Notice', 'Holiday Punch'],
              PDA: null
             }
-var engineer = {primary: ['Shotgun', 'Frontier Justice', 'Widowmaker', 'Pomson 6000', 'Rescue Ranger', 'Panic Attack'],
+var engineer = {name: 'Engineer',
+                primary: ['Shotgun', 'Frontier Justice', 'Widowmaker', 'Pomson 6000', 'Rescue Ranger', 'Panic Attack'],
                 secondary: ['Pistol', 'Wrangler', 'Short Circuit'],
                 melee: ['Wrench', 'Gunslinger', 'Southern Hospitality', 'Jag', 'Eureka Effect'],
                 PDA: null
               }
-var medic = {primary: ['Syringe Gun', 'Blutsauger', 'Crusader\'s Crossbow', 'Overdose'],
+var medic = {name: 'Medic',
+             primary: ['Syringe Gun', 'Blutsauger', 'Crusader\'s Crossbow', 'Overdose'],
              secondary: ['Medi Gun', 'Kritzkrieg', 'Quick-Fix', 'Vaccinator'],
              melee: ['Bonesaw', 'Ubersaw', 'Vita-Saw', 'Amputator', 'Solemn Vow'],
              PDA: null
             }
-var sniper = {primary: ['Sniper Rifle', 'Huntsman', 'Sydney Sleeper', 'Bazaar Bargain', 'Machina', 'Hitman\'s Heatmaker', 'Classic'],
+var sniper = {name: 'Sniper',
+              primary: ['Sniper Rifle', 'Huntsman', 'Sydney Sleeper', 'Bazaar Bargain', 'Machina', 'Hitman\'s Heatmaker', 'Classic'],
               secondary: ['SMG', 'Cleaner\'s Carbine', 'Jarate', 'Razorback', 'Darwin\'s Danger Shield', 'Cozy Camper'],
               melee: ['Kukri', 'Tribalman\'s Shiv', 'Bushwacka', 'Shahanshah'],
               PDA: null
             }
-var spy = {primary: ['Revolver', 'Big Kill', 'Ambassador', 'L\'Etranger', 'Enforcer', 'Diamondback'],
+var spy = {name: 'Spy',
+           primary: ['Revolver', 'Ambassador', 'L\'Etranger', 'Enforcer', 'Diamondback'],
            secondary: ['Sapper', 'Red-Tape Recorder'],
            melee: ['Knife', 'Your Eternal Reward', 'Conniver\'s Kunai', 'Big Earner', 'Spy-cicle'],
            PDA: ['Invis Watch', 'Cloak and Dagger', 'Dead Ringer']
